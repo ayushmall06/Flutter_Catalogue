c81c2bb61aaa4ef5e76f47b189cce8003cd2d4c0
Ayush Mall <ayushmall@Ayushs-MacBook-Pro.local>
2021-05-19 20:06:59 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/Home_Page.dart';
+import 'package:flutter_catalogue/pages/Home_Page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
-  
 }
 
-
-
-class MyApp  extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       home: HomePage()
-       ); 
+      themeMode: ThemeMode.system,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login":(context) => LoginPage()
+      },
+    );
   }
 }
